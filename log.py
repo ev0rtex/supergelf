551c1dd7a8ee9c8a07b629c6783b8e442c013321
David Warkentin <ev0rtex@gmail.com>
2018-07-10 14:30:11 -0600

    Cleaning up debug stuff

--- log.py
+++ log.py
@@ -2,7 +2,6 @@
 import sys
 import os.path as osp
 import logging
-# import json
 from os import environ as env
 
 from pygelf import GelfUdpHandler
@@ -28,13 +27,6 @@ def events(stdin, stdout):
         payload = stdin.read(int(headers['len']))
         h, _, d = payload.partition('\n')
 
-        # # DEBUG
-        # with open('/tmp/supervisor_gelf', 'a') as out:
-        #     out.write("---------------------------------------------------------------\n")
-        #     out.write("HEADERS: {}\n".format(json.dumps(headers)))
-        #     out.write("PAYLOAD: {}\n".format(json.dumps(payload)))
-        # # DEBUG
-
         yield headers, {'headers': extract_kv(h), 'data': d}
 
         # Signal success
@@ -52,12 +44,6 @@ def main():
     logger = logging.getLogger()
     logger.addHandler(gelf_handler)
 
-    # # DEBUG
-    # with open('/tmp/supervisor_gelf', 'a') as out:
-    #     out.write("\n")
-    #     out.write("CONFIG: {}:{}\n".format(env.get('GRAYLOG_HOST'), env.get('GRAYLOG_PORT')))
-    # # DEBUG
-
     # Default record info
     defaults = {
         'name': 'supervisor_gelf',
@@ -71,25 +57,14 @@ def main():
 
     # Log each event
     for headers, event in events(sys.stdin, sys.stdout):
-        #record = logging.makeLogRecord(merged(defaults, {
         record = merged(defaults, {
                 'msg': "Event \"{eventname}\" triggered for process \"{processname}\"".format(**merged(headers, event['headers'])),
                 'data': event['data'],
                 'eventname': headers['eventname']
             },
             event['headers']
-        )#)
+        )
 
-        # # DEBUG
-        # with open('/tmp/supervisor_gelf', 'a') as out:
-        #     out.write("\n")
-        #     out.write("HEADERS: {}\n".format(json.dumps(headers)))
-        #     out.write("EVENT:   {}\n".format(json.dumps(event)))
-        #     out.write("RECORD:  {}\n".format(record))
-        #     out.write("RECORD:  {}\n".format(logging.makeLogRecord(record)))
-        # # DEBUG
-
-        #logger.handle(record)
         logger.handle(logging.makeLogRecord(record))
 
 if __name__ == '__main__':
